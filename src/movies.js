890055a31411133388842e052b2cf90c34a9d68c
Anton Zyrianov <a.zyrianov.z@gmail.com>
2022-07-27 11:21:38 +0200

    solved lab

--- src/movies.js
+++ src/movies.js
@@ -1,3 +1,6 @@
+// import { movies } from './data.js';
+// const moviesArr = require("./data");
+
 // Iteration 1: All directors? - Get the array of all directors.
 // _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors.
 // How could you "clean" a bit this array and make it unified (without duplicates)?
@@ -7,7 +10,12 @@ function getAllDirectors(moviesArray) {
       return element.director;
    });
 
-   return allDirectors;
+   // Bonus
+   const clearDirectors = allDirectors.filter((element, index) => {
+      return allDirectors.indexOf(element) === index;
+   });
+
+   return clearDirectors;
 
 }
 
@@ -31,40 +39,6 @@ function howManyMovies(moviesArray) {
 
 }
 
-
-// const mov = [
-//    {
-//      title: 'The Shawshank Redemption',
-//      year: 1994,
-//      director: 'Frank Darabont',
-//      duration: '2h 22min',
-//      genre: ['Crime', 'Drama'],
-//      score: 9.3
-//    },
-//    {
-//      title: 'The Godfather',
-//      year: 1972,
-//      director: 'Francis Ford Coppola',
-//      duration: '2h 55min',
-//      genre: ['Crime', 'Drama'],
-//      score: 9.2
-//    },
-//    {
-//      title: 'The Godfather: Part II',
-//      year: 1974,
-//      director: 'Francis Ford Coppola',
-//      duration: '3h 22min',
-//      genre: ['Crime', 'Drama'],
-//      score: 9
-//    },
-//    {
-//      title: 'The Dark Knight',
-//      year: 2008,
-//      director: 'Christopher Nolan',
-//      duration: '2h 32min',
-//      genre: ['Action', 'Crime', 'Drama', 'Thriller'],
-//      score: 9
-//    }];
 // Iteration 3: All scores average - Get the average of all scores with 2 decimals
 function scoresAverage(moviesArray) {
 
@@ -77,39 +51,33 @@ function scoresAverage(moviesArray) {
 
    const avgScore = sumScore / moviesArray.length;
 
-   if(!avgScore.length){
+
+   if (moviesArray.length === 0) {
       return 0;
-   }else {
-      // return Math.round(avgScore) / 100;
+   } else {
       return avgScore.toFixed(2);
-   }
+   };
+
 
 }
 
-// console.log(scoresAverage(mov));
+
 
 // Iteration 4: Drama movies - Get the average of Drama Movies
 function dramaMoviesScore(moviesArray) {
 
-   const sumScore = moviesArray.reduce((totalScore, element) => {
+   const sumScore = moviesArray.filter((element) => {
 
-      const isDrama = element.genre.includes('Drama');
-      if(isDrama){
-         totalScore += element.score;
-         return totalScore;
-      }
+      return element.genre.includes('Drama');
 
-   }, 0);
+   });
 
-   const avgScore = sumScore / moviesArray.length;
+   const avgScore = scoresAverage(sumScore);
 
-   if(!avgScore.length){
-      return 0;
-   }else {
-      return Math.round(avgScore) / 100;
-   }
+   return avgScore;
 
 }
+
 
 // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
 function orderByYear(moviesArray) {
@@ -135,8 +103,34 @@ function orderAlphabetically(moviesArray) {
 
 }
 
+function newTimeDuration(time){
+   const hours = time.split('h');
+   const hoursToMinutes = hours[0] * 60;
+
+   const minutes = parseInt(hours[1]);
+
+   const hoursMinutes = hoursToMinutes + minutes;
+
+   return hoursMinutes;
+}
+
 // BONUS - Iteration 7: Time Format - Turn duration of the movies from hours to minutes
-function turnHoursToMinutes(moviesArray) {}
+function turnHoursToMinutes(moviesArray) {
+   
+   const changeHourseToMinutes = moviesArray.map((element) => {
+
+      const newTimeArray = {...element};
+
+      newTimeArray.duration = newTimeDuration(element.duration);
+
+      return newTimeArray;
+   });
+
+   return changeHourseToMinutes;
+}
+
+console.log(turnHoursToMinutes(movies));
+
 
 // BONUS - Iteration 8: Best yearly score average - Best yearly score average
 function bestYearAvg(moviesArray) {}
